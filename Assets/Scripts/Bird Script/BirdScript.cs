aba040db3ccbe6fa27ad2698694bf1783d12cd69
Ivan Brygar <smitbmx@gmail.com>
2021-01-23 21:39:52 +0800

    Added flap functionality.

--- Assets/Scripts/Bird Script/BirdScript.cs
+++ Assets/Scripts/Bird Script/BirdScript.cs
@@ -12,12 +12,21 @@ public class BirdScript : MonoBehaviour
     [SerializeField]
     private Animator anim;
 
-    private float forwardSpeed;
-    private float bounceSpeed;
+    private float forwardSpeed = 3f;
+    private float bounceSpeed = 4f;
+
+    private bool didFlap;
+
+    public bool isAlive;
 
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
 
+        isAlive = true;
     }
 
     // Start is called before the first frame update
@@ -30,5 +39,27 @@ public class BirdScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (isAlive)
+        {
+            Vector2 temp = transform.position;
+            temp.x += forwardSpeed * Time.deltaTime;
+            transform.position = temp;
+
+            if (didFlap)
+            {
+                didFlap = false;
+                myRigidBody.velocity = new Vector2(0, bounceSpeed);
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+
+    public void FlapTheBird()
+    {
+        didFlap = true;
     }
 }
