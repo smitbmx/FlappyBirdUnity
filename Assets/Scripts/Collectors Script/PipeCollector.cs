0d80efc6c5399e962d9e7141ee0d6e4c1248ae52
Ivan Brygar <smitbmx@gmail.com>
2021-01-26 06:15:12 +0800

    Added OnTriggerEnter2D for the PipeCollector for pipe respawn.

--- Assets/Scripts/Collectors Script/PipeCollector.cs
+++ Assets/Scripts/Collectors Script/PipeCollector.cs
@@ -21,11 +21,30 @@ public class PipeCollector : MonoBehaviour
             temp.y = Random.Range(pipeMin, pipeMax);
             pipeHolders[i].transform.position = temp;
         }
+
+        lastPipesX = pipeHolders[0].transform.position.x;
+
+        for (int i = 1; i < pipeHolders.Length; i++)
+        {
+            if (lastPipesX < pipeHolders[i].transform.position.x)
+            {
+                lastPipesX = pipeHolders[i].transform.position.x;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D target)
     {
-        
+        if (target.tag == "PipeHolder")
+        {
+            Vector3 temp = target.transform.position;
+
+            temp.x = lastPipesX + pipeDistance;
+            temp.y = Random.Range(pipeMin, pipeMax);
+
+            target.transform.position = temp;
+
+            lastPipesX = temp.x;
+        }
     }
 }
