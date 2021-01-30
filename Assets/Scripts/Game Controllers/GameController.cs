c243a4d7701e3bc68340d2b53fe1c25b835fa383
Ivan Brygar <smitbmx@gmail.com>
2021-01-30 13:51:48 +0800

    Implemented GameController

--- Assets/Scripts/Game Controllers/GameController.cs
+++ Assets/Scripts/Game Controllers/GameController.cs
@@ -4,15 +4,87 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController instance;
+
+    private const string HIGH_SCORE = "High Score";
+    private const string SELECTED_BIRD = "Selected Bird";
+    private const string GREEN_BIRD = "Green Bird";
+    private const string RED_BIRD = "Red Bird";
+
+    private void Awake()
+    {
+        MakeSingleton();
+        IsTheGameStartedForTheFirstTime();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    void MakeSingleton()
     {
-        
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+    }
+
+    void IsTheGameStartedForTheFirstTime()
+    {
+        if (!PlayerPrefs.HasKey("IsTheGameStartedForTheFirstTime"))
+        {
+            PlayerPrefs.SetInt(HIGH_SCORE, 0);
+            PlayerPrefs.SetInt(SELECTED_BIRD, 0);
+            PlayerPrefs.SetInt(GREEN_BIRD, 0);
+            PlayerPrefs.SetInt(RED_BIRD, 0);
+            PlayerPrefs.SetInt("IsTheGameStartedForTheFirstTime", 0);
+        }
+    }
+
+    public void SetHighScore(int score)
+    {
+        PlayerPrefs.SetInt(HIGH_SCORE, score);
+    }
+
+    public int GetHighScore()
+    {
+        return PlayerPrefs.GetInt(HIGH_SCORE);
+    }
+
+    public void SetSelectedBird(int selectedBird)
+    {
+        PlayerPrefs.SetInt(SELECTED_BIRD, selectedBird);
+    }
+
+    public int GetSelectedBird()
+    {
+        return PlayerPrefs.GetInt(SELECTED_BIRD);
+    }
+
+    public void UnlockGreenBird()
+    {
+        PlayerPrefs.SetInt(GREEN_BIRD, 1);
+    }
+
+    public int IsGreenBirdUnlocked()
+    {
+        return PlayerPrefs.GetInt(GREEN_BIRD);
+    }
+
+    public void UnlockRedBird()
+    {
+        PlayerPrefs.SetInt(RED_BIRD, 1);
+    }
+
+    public int IsRedBirdUnlocked()
+    {
+        return PlayerPrefs.GetInt(RED_BIRD);
     }
 }
