a43948a93f5350868d766c34256834719c23cbaf
Ivan Brygar <smitbmx@gmail.com>
2021-01-30 14:58:20 +0800

    Added basic logic to the MenuController and assigned menu birds to it.

--- Assets/Scripts/Game Controllers/MenuController.cs
+++ Assets/Scripts/Game Controllers/MenuController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
+    public static MenuController instance;
+
+    [SerializeField]
+    private GameObject[] birds;
+
+    private bool isGreenBirdUnlocked;
+    private bool isRedBirdUnlocked;
+
+    private void Awake()
+    {
+        MakeInstance();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        birds[GameController.instance.GetSelectedBird()].SetActive(true);
+
+        CheckIfBirdsAreUnlocked();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    void CheckIfBirdsAreUnlocked()
+    {
+        if (GameController.instance.IsRedBirdUnlocked() == 1)
+        {
+            isRedBirdUnlocked = true;
+        }
+
+        if (GameController.instance.IsGreenBirdUnlocked() == 1)
+        {
+            isGreenBirdUnlocked = true;
+        }
     }
 }
