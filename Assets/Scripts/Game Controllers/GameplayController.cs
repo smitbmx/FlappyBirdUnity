2abd4735ce13abfa99cccf4d58300403cd09b6e5
Ivan Brygar <smitbmx@gmail.com>
2021-01-31 19:45:34 +0800

    Assigned UI elements to the Gameplay Controller

--- Assets/Scripts/Game Controllers/GameplayController.cs
+++ Assets/Scripts/Game Controllers/GameplayController.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameplayController : MonoBehaviour
 {
+    public static GameplayController instance;
+
+    [SerializeField]
+    private Text scoreText, endScore, bestScore, gameOverText;
+
+    [SerializeField]
+    private Button restartGameButton, instructionsButton;
+
+    [SerializeField]
+    private GameObject pausePanel;
+
+    [SerializeField]
+    private GameObject[] birds;
+
+    [SerializeField]
+    private Sprite[] medals;
+
+    [SerializeField]
+    private Image medalImage;
+
+    private void Awake()
+    {
+        MakeInstance();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void MakeInstance()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
     }
 }
